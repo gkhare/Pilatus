8cb07cf1e3d459b2d0b5378fd8cc03267b128d01
Gaurav Khare <gaurav.khare.1@gmail.com>
2012-08-13 17:34:35 +0530

    fixed the JSLoop to support inheritance

--- JSAsync-serialLoop/jsloop.js
+++ JSAsync-serialLoop/jsloop.js
@@ -1,62 +1,146 @@
-function loop(repetitions, JSLoopBaseSubClass) {
-	var keepRunning = true;
-	var free = true;
-	var counter = 0;
+JSLoopBase = function() {
 	
-	var loopRunning = setInterval(function() {
-		if (counter < repetitions) {
-			if (free) {
-				free = false;
-				//run method
-				JSLoopBaseSubClass.run();
+	this.TYPE = {"simple": 1, "timeout": 2, "interval": 3};
+		
+	this.runningDone = false;
+	
+	this.runType = this.TYPE.simple;
+	
+	this.time = 0;
+	
+	this.isRunningDone = function() {
+		return this.runningDone;
+	};
+
+	this.setRunningDone = function(val) {
+		this.runningDone = val;
+	};
+	
+	this.setRunType = function(type) {
+		this.runType = type;
+	};
+	
+	this.setTime = function(time) {
+		this.time = time;
+	};
+	
+	this.getRunType = function() {
+		return this.runType;
+	};
+	
+	this.getTime = function() {
+		return this.time;
+	};
+	
+	
+	this.execute = function() {
+		// override this function
+		return true;
+	};
+	
+	this.run = function() {
+		switch (this.getRunType()) {
+		case this.TYPE.simple:
+			this.runSimple();
+			break;
+		case this.TYPE.timeout:
+			if (this.getTime() == 0) {
+				console.log("provide a timeout for setTimeOut");
+				break;
+			}
+			this.runTimeOut(this.getTime());
+			break;
+		case this.TYPE.interval:
+			if (this.getTime() == 0) {
+				console.log("provide a timeout for setInterval");
+				break;
+			}
+			this.runInterval(this.getTime());
+			break;
+		default:
+			break;
+		}
+	};
+	
+	this.runSimple = function() {
+		this.execute();
+		this.setRunningDone(true);
+	};
+	
+	this.runTimeOut = function(timeout) {
+		var base = this;
+		setTimeout(function() {
+			base.execute();
+			base.setRunningDone(true);
+		}, timeout);
+	};
+	
+	this.runInterval = function(interval) {
+		var base = this;
+		var done = setInterval(function() {
+			if(base.execute()) {
+				base.setRunningDone(true);
+				clearInterval(done);					
+			}
+		}, interval);
+	};
+	
+	this.createObject = function(child) {
+		child.prototype = this;
+		var instance = new child();
+		return instance;
+	};
+	
+	this.loop = function(repetitions, JSLoopBaseSubClass) {
+		var keepRunning = true;
+		var free = true;
+		var counter = 0;
+		
+		var loopRunning = setInterval(function() {
+			if (counter < repetitions) {
+				if (free) {
+					free = false;
+					//run method
+					JSLoopBaseSubClass.run();
+				} else {
+					console.log('waiting for loop '+counter+' to end...');
+				}
+				if (JSLoopBaseSubClass.isRunningDone()) {
+					console.log('Done for loop '+counter);
+					JSLoopBaseSubClass.setRunningDone(false);
+					free = true;
+					counter++;
+				}
 			} else {
-				console.log('waiting for loop '+counter+' to end...');
+				keepRunning = false;
 			}
-			if (JSLoopBaseSubClass.isRunningDone()) {
-				console.log('Done for loop '+counter);
-				free = true;
-				counter++;
+			if (!keepRunning) {
+				clearInterval(loopRunning);
 			}
-		} else {
-			keepRunning = false;
-		}
-		if (!keepRunning) {
-			clearInterval(loopRunning);
-		}
-	},10);
+		},10);
+	};
 }
 
-JSLoopBase = {
-		runningDone: false,
-		
-		isRunningDone: function() {
-			return this.runningDone;
-		},
-
-		setRunningDone: function(val) {
-			this.runningDone = val;
-		},
-		
-		execute: function() {
-			// override this function
-		},
-		
-		run: function() {
-			this.execute();
-			this.setRunningDone(true);
-		}
-},
-
 //Create your class as Test overriding the execute method
+Base = new JSLoopBase();
 
-Test = {
-		execute: function() {
-			setTimeout(function() {
-				console.log("doing something");
-			}, 2000);
-		}
-}.prototype = JSLoopBase;
+Test = function() {
+	
+	this.getRunType = function() {
+		return Base.TYPE.timeout;
+	};
+	
+	this.getTime = function() {
+		return 2000;
+	};
+		
+	this.execute = function() {
+		console.log("doing something");
+	};
+}
+
+Base.loop(3, Base.createObject(Test));
 
 //call the method as mentioned below
 
-loop(3, Test);
+//loop(3, Test);
